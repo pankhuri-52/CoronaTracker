43a1008bdb754b629c4153bfa572af5b04bca4c5
Pankhuri <pankhuritrikha@gmail.com>
2020-06-20 23:40:55 +0530

    Bar chart generated

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -4,7 +4,7 @@ import {Line, Bar} from 'react-chartjs-2';
 
 import styles from './Chart.module.css';
 
-const Chart = () => {
+const Chart = ({data : {confirmed, recovered, deaths}, country}) => {
     
     const [dailyData, setDailyData ] = useState([]);
 
@@ -13,7 +13,7 @@ const Chart = () => {
            setDailyData( await fetchDailyData());
         }
         fetchAPI();
-    });
+    }, []);
 
     const lineChart = (
         dailyData.length ?   // if daily data is available then show it in form of graph else return null
@@ -32,11 +32,33 @@ const Chart = () => {
                 fill : true
             }]
         }} />) : null
-    )
+    );
+
+    console.log(confirmed, recovered, deaths);
+
+    const barChart = (
+        confirmed ? 
+        (
+            <Bar 
+             data = {{
+                labels : ['Infected' , 'Recovered', 'Deaths'],
+                datasets : [{
+                    label : 'People',
+                    backgroundColor : ['rgba(0,0,255,0.5)','rgba(0,255,0,0.5)','rgba(255,0,0,0.5)'],
+                    data : [confirmed.value, recovered.value, deaths.value]
+                }]
+             }} 
+             options ={{
+                 legend : {display : false},
+                 title : { display : true, text : `Current State in ${country}`}
+             }}
+             />
+        ) : null
+    );
 
     return(
         <div className={styles.container}>
-            {lineChart}
+            {country ? barChart : lineChart}
         </div>
     );
 }
